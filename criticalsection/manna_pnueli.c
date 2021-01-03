8ce3bb0f6a66338a6eebfc4d6e183c59a970d2d2
Gabriela Midori Restani <midori.restani@gmail.com>
2021-01-02 22:28:59 -0300

    :sparkles: add server_process code and add client_process critical section

--- criticalsection/manna_pnueli.c
+++ criticalsection/manna_pnueli.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
-long count = 0, local = 0, response = 0, request =0;
+long count = 0, sum = 0, respond = 0, request =0;
 
 void *client_process(void* i){
+    long thread_id = (long) i;
+    long local = 0;
+    while(respond!=thread_id){
+        request = thread_id;
+    }
+    local = sum;
+    sleep(rand()%2);
+    sum = local + 1;
 
 }
 
 void *server_process(void* i){
+    while(request==0);
     
+    respond = request;
+    
+    while(respond!=0);
+
+    request = 0;
 }
 
 int main(int argc, char const *argv[])
