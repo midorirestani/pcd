29e34339653b4f639ea8d942c40ed54c10dc4b5d
Gabriela Midori Restani <midori.restani@gmail.com>
2021-01-02 22:07:48 -0300

    :sparkles: add server and client process functions

--- criticalsection/manna_pnueli.c
+++ criticalsection/manna_pnueli.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+
+long count = 0, local = 0, response = 0, request =0;
+
+void *client_process(void* i){
+
+}
+
+void *server_process(void* i){
+    
+}
 
 int main(int argc, char const *argv[])
 {
