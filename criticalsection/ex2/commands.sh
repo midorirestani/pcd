8a63b51a619d5fe3d14cef894fb88842efe1ba69
Klara Kague <kskague>
2021-01-09 17:08:47 -0500

    Fixed openmp code and did ex2 a and b

--- criticalsection/ex2/commands.sh
+++ criticalsection/ex2/commands.sh
@@ -1,7 +1,7 @@
 alias ompa-compile="gcc -o game_of_life_openmp_a -fopenmp game_of_life_openmp_a.c"
 alias ompa-run="./game_of_life_openmp_a"
 
-alias ompb-compile="gcc -o game_of_life_openmp_b -fopenmp game_of_life_openmp.c"
+alias ompb-compile="gcc -o game_of_life_openmp_b -fopenmp game_of_life_openmp_b.c"
 alias ompb-run="./game_of_life_openmp_b"
 
 alias ompc-compile="gcc -o game_of_life_openmp_c -fopenmp game_of_life_open_mp_c.c"
