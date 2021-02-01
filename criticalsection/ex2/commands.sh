75a88f3cb63d46b71ab30c3fd125507dfbac09e9
Klara Kague <kskague>
2021-02-01 17:45:56 -0500

    Adds command file to activity 03

--- criticalsection/ex2/commands.sh
+++ criticalsection/ex2/commands.sh
@@ -4,5 +4,5 @@ alias ompa-run="./game_of_life_openmp_a"
 alias ompb-compile="gcc -o game_of_life_openmp_b -fopenmp game_of_life_openmp_b.c"
 alias ompb-run="./game_of_life_openmp_b"
 
-alias ompc-compile="gcc -o game_of_life_openmp_c -fopenmp game_of_life_open_mp_c.c"
+alias ompc-compile="gcc -o game_of_life_openmp_c -fopenmp game_of_life_openmp_c.c"
 alias ompc-run="./game_of_life_openmp_c"
